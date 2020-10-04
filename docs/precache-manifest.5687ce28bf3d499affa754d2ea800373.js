729722eea5f137a66444afea8cec0e841d1928eb
EmisoraSanPablo <estereosanpablo@gmail.com>
2020-10-04 10:45:01 -0500

    docs agregado

---
+++ docs/precache-manifest.5687ce28bf3d499affa754d2ea800373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfb20fce01c9f59793e9fcd714b7cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85e050c527096d85f2e",
+    "url": "/static/css/main.80ac59b7.chunk.css"
+  },
+  {
+    "revision": "3884e4cb4096bc69dd1b",
+    "url": "/static/js/2.6a47513e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a47513e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85e050c527096d85f2e",
+    "url": "/static/js/main.d91aa56e.chunk.js"
+  },
+  {
+    "revision": "21647c64104c6f40a2c0",
+    "url": "/static/js/runtime-main.199852c7.js"
+  }
+]);
